8c2f0d8ddc80f3ec7bddc54c662386f7d570cec6
Jaylan <prosnipa1221@gmail.com>
2021-10-27 20:30:46 -0500

    Final Commit

--- GestureRec/ViewController.swift
+++ GestureRec/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var ramen: UIImageView!
+    @IBOutlet weak var rice: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.cyan
     }
 
-
+    @IBAction func tapScreenAction(_ sender: UITapGestureRecognizer) {
+        var tapLoc = sender.location(in: view)
+        ramen.center = tapLoc
+    }
+    @IBAction func panAction(_ sender: UIPanGestureRecognizer) {
+       var panLoc =  sender.location(in: view)
+        rice.center = panLoc
+    }
+    
 }
 
